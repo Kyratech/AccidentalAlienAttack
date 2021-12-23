90389dd011b0458d0f21e801a97a3207334d9ae9
Matthew Langford <ml2g14@soton.ac.uk>
2021-12-23 21:04:10 +0000

    Start version control

---
+++ src/animationUtils.lua
@@ -0,0 +1,25 @@
+function animate(gameObject, animation)
+	gameObject.ani.delayCounter = gameObject.ani.delayCounter + 1
+	
+	if gameObject.ani.delayCounter > animation.frameDelay then
+		local frame = gameObject.ani.currentCounter % animation.length
+		gameObject.ani.currentCounter = frame + 1
+		gameObject.ani.currentFrame = animation.sprites[gameObject.ani.currentCounter]
+		gameObject.ani.delayCounter = 0
+	end
+end
+
+function animateOneshot(gameObject, animation)
+	gameObject.ani.delayCounter = gameObject.ani.delayCounter + 1
+	
+	if gameObject.ani.delayCounter > animation.frameDelay then
+		gameObject.ani.currentCounter = gameObject.ani.currentCounter + 1
+		
+		if gameObject.ani.currentCounter > animation.length + 1 then
+			gameObject:disable()
+		else
+			gameObject.ani.currentFrame = animation.sprites[gameObject.ani.currentCounter]
+			gameObject.ani.delayCounter = 0
+		end
+	end
+end
