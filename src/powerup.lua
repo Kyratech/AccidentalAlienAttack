919108e2e0ee70e40c0ec3b9e5bce5923208bd62
Katherine Langford <katherinelangford142@gmail.com>
2022-02-05 19:41:46 +0000

    Eliminate duplicate powerup code

--- src/powerup.lua
+++ src/powerup.lua
@@ -7,7 +7,6 @@ MaxFrequencyScore = 5
 
 ActivateRandomPowerup = function(x, y)
 	local powerupIndex = math.random(MaxFrequencyScore)
-	DrawDebug(powerupIndex)
 	if powerupIndex <= PowerupFrequency.shield then
 		ShieldPowerup:enable(x, y)
 	elseif powerupIndex <= PowerupFrequency.extraLife then
@@ -27,77 +26,18 @@ PowerupConsts = {
 }
 
 CreateExtraLifePowerup = function ()
-	return {
-		x = -20,
-		y = -20,
-		w = PowerupConsts.width * TilePx,
-		h = PowerupConsts.height * TilePx,
-		active = false,
-		speed = 0,
-		bounces = 0,
-		countdown = 0,
-		update = function (self)
-			if self.active == true then
-				if self.y + self.h ~= GroundY and self.speed <= PowerupConsts.maxSpeed then
-					self.speed = self.speed + PowerupConsts.acceleration
-				end
-
-				self.countdown = self.countdown - 1
-				
-				if self.countdown <= 0 then
-					self:disable()
-				end
-
-				self.y = self.y + self.speed
-			end
-		end,
-		draw = function (self)
-			if self.active == true then
-				if self.countdown > PowerupConsts.lifetimeFlash or self.countdown % 4 < 2 then
-					spr(
-						256,
-						self.x,
-						self.y,
-						PowerupConsts.clrIndex
-					)
-				end
-			end
-		end,
-		checkCollision = function (self)
-			if self.y + self.h >= GroundY then
-				if self.bounces < PowerupConsts.bounces then
-					self.speed = -self.speed
-					self.bounces = self.bounces + 1
-				else
-					self.speed = 0
-				end
-
-				self.y = GroundY - self.h
-			end
-
-			if Collide(self, Player) then
-				Lives = Lives + 1
-				self:disable()
-			end
-		end,
-		enable = function (self, x, y)
-			self.active = true
-			self.x = x
-			self.y = y
-			self.speed = 0
-			self.bounces = 0
-			self.countdown = PowerupConsts.lifetime
-		end,
-		disable = function (self)
-			self.active = false
-			self.x = -20
-			self.y = -20
-			self.speed = 0
-		end
-	}
+	return CreatePowerup(256, function()
+		Lives = Lives + 1
+	end)
 end
 
 CreateShieldPowerup = function ()
+	return CreatePowerup(258, function()
+		Player:activateShield(PlayerConsts.powerupShieldLength)
+	end)
+end
+
+CreatePowerup = function (spriteIndex, collectionFunction)
 	return {
 		x = -20,
 		y = -20,
@@ -126,7 +66,7 @@ CreateShieldPowerup = function ()
 			if self.active == true then
 				if self.countdown > PowerupConsts.lifetimeFlash or self.countdown % 4 < 2 then
 					spr(
-						258,
+						spriteIndex,
 						self.x,
 						self.y,
 						PowerupConsts.clrIndex
@@ -147,7 +87,7 @@ CreateShieldPowerup = function ()
 			end
 
 			if Collide(self, Player) then
-				Player:activateShield(PlayerConsts.powerupShieldLength)
+				collectionFunction()
 				self:disable()
 			end
 		end,
