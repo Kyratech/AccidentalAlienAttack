5d8099596edd974ec910432904ad26bfd0f3f0eb
Matthew Langford <ml2g14@soton.ac.uk>
2022-04-15 18:26:53 +0100

    Improved the gameover screen

--- src/loopGameOver.lua
+++ src/loopGameOver.lua
@@ -1,53 +1,53 @@
+GameOverPages = {
+	{
+		draw = function()
+			DialogueObject:draw()
+
+			PrintCustomCentredDecorated("incoming transmission", HalfScreenWidth, 8)
+			PrintCustomCentredDecorated("end transmission", HalfScreenWidth, ScreenHeight - 16)
+
+			DrawButtonPrompt(ButtonIcons.A, "Continue", ScreenWidth - 63, ScreenHeight - 8)
+		end,
+		update = function()
+			DialogueObject:update()
+		end,
+		input = function()
+			if btnp(BtnA) then
+				GameOverPage = GameOverPage + 1
+			end
+		end
+	},
+	{
+		draw = function()
+			PrintCentred("Final score", HalfScreenWidth, 60, 12)
+			PrintCentred(Score, HalfScreenWidth, 68, 3)
+
+			PrintCustomCentredDecorated("performance review", HalfScreenWidth, 8)
+			PrintCustomCentredDecorated("performance review", HalfScreenWidth, ScreenHeight - 16)
+
+			DrawButtonPrompt(ButtonIcons.A, "Finish", ScreenWidth - 56, ScreenHeight - 8)
+		end,
+		update = function()
+		end,
+		input = function()
+			if btnp(BtnA) then
+				TitleScreen()
+			end
+		end
+	}
+}
+
 function GameOverLoop()
 	cls(BgColour)
-	GoInput()
-	GoUpdate()
-	GoDraw()
+	GameOverPages[GameOverPage]:input()
+	GameOverPages[GameOverPage]:update()
+	GameOverPages[GameOverPage]:draw()
 end
 
 function GameOver(script, numberOfLines)
 	GameState = StateGameOver
 
-	local columnWidth = 20
+	GameOverPage = 1
 
-	DialogueObject = CreateDialogueScreen(script, numberOfLines, columnWidth)
-
-	ScoreText = {
-		text = "You scored " .. Score .. " points!",
-		y = 110,
-		draw = function (self)
-			PrintCentred(self.text, 120, self.y, 12)
-		end
-	}
-
-	RestartText = {
-		text = "Press [A] to play again.",
-		y = 120,
-		draw = function (self)
-			PrintCentred(self.text, 120, self.y, 12)
-		end
-	}
-end
-
-function GoInput()
-	if btn(BtnA) then
-		GameState = StateDialogue
-		DialogueInit(
-			ScriptIntro,
-			3,
-			function()
-				GameState = StatePlaying
-				StartGame()
-			end)
-	end
-end
-
-function GoUpdate()
-	DialogueObject:update()
-end
-
-function GoDraw()
-	DialogueObject:draw()
-	ScoreText:draw()
-	RestartText:draw()
+	DialogueObject = CreateDialogueScreen(script, numberOfLines, 20)
 end
